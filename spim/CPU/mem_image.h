e56b4a087d74c2ad54c4bf8b7c7378969cf15a23
Jareth Gomes <jarethgomes@gmail.com>
2022-09-07 08:55:22 -0500

    Fixed segfault caused by reading pointers before initialization

--- spim/CPU/mem_image.h
+++ spim/CPU/mem_image.h
@@ -11,43 +11,42 @@ typedef int32 /*@alt unsigned int @*/ mem_word;
 
 typedef struct memimage {
 	/* The text segment. */
-	instruction **text_seg;
-	unsigned *text_prof;
-	int text_modified;		/* => text segment was written */
-	mem_addr text_top;
+	instruction **text_seg = 0;
+	unsigned *text_prof = 0;
+	int text_modified = 0;		/* => text segment was written */
+	mem_addr text_top = 0;
 
 	/* The data segment. */
-	mem_word *data_seg;
-	bool data_modified;		/* => a data segment was written */
-	short *data_seg_h;		/* Points to same vector as DATA_SEG */
-	BYTE_TYPE *data_seg_b;		/* Ditto */
-	mem_addr data_top;
-	mem_addr gp_midpoint;		/* Middle of $gp area */
+	mem_word *data_seg = 0;
+	bool data_modified = 0;		/* => a data segment was written */
+	short *data_seg_h = 0;		/* Points to same vector as DATA_SEG */
+	BYTE_TYPE *data_seg_b = 0;		/* Ditto */
+	mem_addr data_top = 0;
+	mem_addr gp_midpoint = 0;		/* Middle of $gp area */
 
 	/* The stack segment. */
-	mem_word *stack_seg;
-	short *stack_seg_h;		/* Points to same vector as STACK_SEG */
-	BYTE_TYPE *stack_seg_b;		/* Ditto */
-	mem_addr stack_bot;
+	mem_word *stack_seg = 0;
+	short *stack_seg_h = 0;		/* Points to same vector as STACK_SEG */
+	BYTE_TYPE *stack_seg_b = 0;		/* Ditto */
+	mem_addr stack_bot = 0;
 
 	/* Used for SPIMbot stuff. */
-	mem_word *special_seg;
-	short *special_seg_h;
-	BYTE_TYPE *special_seg_b;
+	mem_word *special_seg = 0;
+	short *special_seg_h = 0;
+	BYTE_TYPE *special_seg_b = 0;
 
 	/* The kernel text segment. */
-	instruction **k_text_seg;
-	unsigned *k_text_prof;
-	mem_addr k_text_top;
+	instruction **k_text_seg = 0;
+	unsigned *k_text_prof = 0;
+	mem_addr k_text_top = 0;
 
 	/* The kernel data segment. */
-	mem_word *k_data_seg;
-	short *k_data_seg_h;
-	BYTE_TYPE *k_data_seg_b;
-	mem_addr k_data_top;
+	mem_word *k_data_seg = 0;
+	short *k_data_seg_h = 0;
+	BYTE_TYPE *k_data_seg_b = 0;
+	mem_addr k_data_top = 0;
 
-	char* prof_file_name;
-
+	char* prof_file_name = 0;
 } mem_image_t;
 
 #endif
